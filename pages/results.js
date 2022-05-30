cd80e1cefb6cb6642de32916ce1b967a06755d7e
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2022-05-30 19:35:06 +0200

    b

--- pages/results.js
+++ pages/results.js
@@ -5,12 +5,11 @@ import axios from "axios";
 import { BASE_URL } from "./constants/api";
 
 export default function Index(props) {
-  // the log here will happen in the browser console
   console.log(props);
 
   return (
     <Layout>
-      <Head title="Next Intro" />
+      <Head title="Results" />
       <Heading title="Results" />
 
       {props.characters.map((character) => {
@@ -21,21 +20,17 @@ export default function Index(props) {
 }
 
 export async function getStaticProps() {
-  // in case there is an error in the API call
-  // we'll send an empty array in as the prop
+
   let characters = [];
 
   try {
     const response = await axios.get(BASE_URL);
-    // the log here will happen on the server, you can check the console in your editor
     console.log(response.data);
-    // the array is on the response.data.results property
     characters = response.data.results;
   } catch (error) {
     console.log(error);
   }
 
-  // the props object we return here will become the props in the component
   return {
     props: {
       characters: characters,
